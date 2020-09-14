1804293823a63ec32bc16cb62e601d68a09ca88f
Mudassar Abbass <mudassar.abbass@student.kyh.se>
2020-09-14 21:22:02 +0200

    Removed spaces

--- src/com/P4Tech/FizzBuzz/Main.java
+++ src/com/P4Tech/FizzBuzz/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-
             if (i == 3) {
                 System.out.println("Fizz");
             } else if (i == 5) {
@@ -13,11 +12,9 @@ public class Main {
                 System.out.println("FizzBuzz");
             } else if (i == 42) {
                 System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
-
             } else {
                 System.out.println(i);
             }
-
         }
     }
 }
